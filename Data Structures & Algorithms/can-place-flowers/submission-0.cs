36ac84e182f9bf3f65a8b1e69cfda17cfb29e68b
Anjaneya <52105174+asp89@users.noreply.github.com>
2026-05-12 10:50:11 +0530

    Add: can-place-flowers - submission-0

---
+++ Data Structures & Algorithms/can-place-flowers/submission-0.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public bool CanPlaceFlowers(int[] flowerbed, int n) {
+        for (int i = 0; i < flowerbed.Length; i++) {
+            if (flowerbed[i] == 0) {
+                bool emptyLeft = (i == 0) || (flowerbed[i - 1] == 0);
+                bool emptyRight = (i == flowerbed.Length - 1) || (flowerbed[i + 1] == 0);
+
+                if (emptyLeft && emptyRight) {
+                    flowerbed[i] = 1;
+                    n--;
+                }
+            }
+
+            if (n <= 0)
+                return true;
+        }
+
+        return n <= 0;
+    }
+}
