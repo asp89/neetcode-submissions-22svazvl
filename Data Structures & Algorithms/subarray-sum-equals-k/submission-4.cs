fc82d89b954a0326ca4eb2a11f8afcbe89d0e97b
Anjaneya <52105174+asp89@users.noreply.github.com>
2026-05-30 00:17:35 +0530

    Add: subarray-sum-equals-k - submission-4

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-4.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int SubarraySum(int[] nums, int k) {
+        var dict = new Dictionary<int, int>();
+        dict[0] = 1;
+        Console.WriteLine(dict.First());
+        int currentSum = 0;
+        int count = 0;
+
+        foreach (int num in nums)
+        {
+            currentSum = currentSum + num;
+
+            if (dict.ContainsKey(currentSum - k))
+                count = count + dict[currentSum - k];
+            
+            if (dict.ContainsKey(currentSum))
+                dict[currentSum]++;
+            else
+                dict[currentSum] = 1;
+        }
+
+        return count;
+    }
+}
