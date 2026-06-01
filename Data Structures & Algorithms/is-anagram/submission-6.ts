7357855837a4ab35c9cbc591e41ea4406b0b8bd4
Anjaneya <52105174+asp89@users.noreply.github.com>
2026-06-01 20:55:19 +0530

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.ts
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        const frequency: number[] = new Array(26).fill(0);
+
+        for (const ch of s)
+            frequency[ch.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+
+        for (const ch of t)
+            frequency[ch.charCodeAt(0) - 'a'.charCodeAt(0)]--;
+
+        for (const freq of frequency) {
+            if (freq != 0) return false;
+        }
+
+        return true;
+    }
+}
