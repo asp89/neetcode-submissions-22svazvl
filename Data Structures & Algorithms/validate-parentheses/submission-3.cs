c0980bfbfd6b9d15b65f129b5a499ce440d9f101
Anjaneya <52105174+asp89@users.noreply.github.com>
2026-06-12 22:21:16 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public bool IsValid(string s) {
+        Dictionary<string, string> map = new() { [")"] = "(", ["}"] = "{", ["]"] = "[" };
+
+        Stack<string> stack = new();
+
+        foreach (char ch in s) {
+            string c = ch.ToString();
+            if (map.ContainsValue(c)) {
+                stack.Push(c);
+            } else {
+                if (stack.Count == 0)
+                    return false;
+
+                if (stack.Pop() != map[c])
+                    return false;
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
