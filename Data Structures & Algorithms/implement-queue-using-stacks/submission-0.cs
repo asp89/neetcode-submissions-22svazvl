e59875165f247f60043d1212b4f2d2b5c4e4642d
Anjaneya <52105174+asp89@users.noreply.github.com>
2026-06-14 22:29:57 +0530

    Add: implement-queue-using-stacks - submission-0

---
+++ Data Structures & Algorithms/implement-queue-using-stacks/submission-0.cs
@@ -0,0 +1,38 @@
+public class MyQueue {
+    Stack<int> inStack = new();
+    Stack<int> outStack = new();
+
+    public MyQueue() {}
+
+    public void Push(int x) {
+        inStack.Push(x);
+    }
+
+    public int Pop() {
+        if (outStack.Count == 0) {
+            while (inStack.Count > 0) outStack.Push(inStack.Pop());
+        }
+        return outStack.Pop();
+    }
+
+    public int Peek() {
+        if (outStack.Count == 0) {
+            while (inStack.Count > 0) outStack.Push(inStack.Pop());
+        }
+
+        return outStack.Peek();
+    }
+
+    public bool Empty() {
+        return (inStack.Count + outStack.Count) == 0;
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.Push(x);
+ * int param_2 = obj.Pop();
+ * int param_3 = obj.Peek();
+ * bool param_4 = obj.Empty();
+ */
