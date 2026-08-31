2fbfa1323fd85b1fe08454c139de68e062781891
Anjaneya <52105174+asp89@users.noreply.github.com>
2026-08-31 23:19:12 +0530

    Add: merge-strings-alternately - submission-0

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-0.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public string MergeAlternately(string word1, string word2) {
+        StringBuilder result = new();
+        int len = Math.Max(word1.Length, word2.Length);
+
+        for (int i = 0; i < len; i++) {
+            if (i < word1.Length)
+                result.Append(word1[i]);
+            if (i < word2.Length)
+                result.Append(word2[i]);
+        }
+
+        return result.ToString();
+    }
+}
