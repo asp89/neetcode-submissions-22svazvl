12c52dbdc5198c05a15b00b3b67721a192d0990c
Anjaneya <52105174+asp89@users.noreply.github.com>
2026-09-19 12:09:44 +0530

    Add: longest-common-prefix - submission-2

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-2.cs
@@ -0,0 +1,53 @@
+public class Solution {
+    public class TrieNode {
+        public TrieNode[] Children = new TrieNode[26];
+        public bool IsWord;
+    }
+
+    public string LongestCommonPrefix(string[] strs) {
+        TrieNode root = new TrieNode();
+
+        // Build Trie
+        foreach (string word in strs) {
+            TrieNode current = root;
+
+            foreach (char c in word) {
+                int index = c - 'a';
+
+                if (current.Children[index] == null)
+                    current.Children[index] = new TrieNode();
+
+                current = current.Children[index];
+            }
+
+            current.IsWord = true;
+        }
+
+        // Find LCP
+        TrieNode node = root;
+        StringBuilder prefix = new StringBuilder();
+
+        while (true) {
+            if (node.IsWord)
+                break;
+
+            int childCount = 0;
+            int nextIndex = -1;
+
+            for (int i = 0; i < 26; i++) {
+                if (node.Children[i] != null) {
+                    childCount++;
+                    nextIndex = i;
+                }
+            }
+
+            if (childCount != 1)
+                break;
+
+            prefix.Append((char)('a' + nextIndex));
+            node = node.Children[nextIndex];
+        }
+
+        return prefix.ToString();
+    }
+}
